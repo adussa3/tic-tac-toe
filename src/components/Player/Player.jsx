a04ac7e34d1d716b945b3cfc32354e726c253944
adussa3 <adussa3@gmail.com>
2024-02-20 23:12:47 -0800

    Added Player Component

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function Player({ initialName = "Player", symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+
+    const handleEditClick = () => {
+        setIsEditing((prevIsEditing) => !prevIsEditing);
+    };
+
+    const handleChange = (event) => {
+        const { value } = event.target;
+        setPlayerName(value);
+    };
+
+    return (
+        <li>
+            <span className="player">
+                {!isEditing && <span className="player-name">{playerName}</span>}
+                {/*
+                    This is a controlled component! It listens to the change of the input and feeding the updated value
+                    back into the input (this is called two-way binding)
+                */}
+                {isEditing && <input type="text" className="player-name" onChange={handleChange} value={playerName} required />}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+    );
+}
