2c223389f48d92ae1987fa7ca64d361c4bbf43f3
adussa3 <adussa3@gmail.com>
2024-02-21 10:50:13 -0800

    Added the disabled attribute when button is clicked

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,25 +1,31 @@
-import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    // RECALL: we DON'T want to directly update the matrix because it's still has the same memory address (this can cause weird bugs)
-    //         instead, we need to return a copy of the matrix with a NEW memory address
-    const handleSelectSquare = (rowIndex, colIndex) => {
-        setGameBoard((prevGameBoard) => {
-            const updatedGameBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-            if (!updatedGameBoard[rowIndex][colIndex]) updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedGameBoard;
-        });
+    // // RECALL: we DON'T want to directly update the matrix because it's still has the same memory address (this can cause weird bugs)
+    // //         instead, we need to return a copy of the matrix with a NEW memory address
+    // const handleSelectSquare = (rowIndex, colIndex) => {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedGameBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
+    //         if (!updatedGameBoard[rowIndex][colIndex]) updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedGameBoard;
+    //     });
 
-        onSelectSquare();
-    };
+    //     onSelectSquare();
+    // };
+
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+        gameBoard[row][col] = player;
+    }
 
     return (
         <ol id="game-board">
@@ -30,7 +36,12 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
                             {row.map((playerSymbol, colIndex) => {
                                 return (
                                     <li key={colIndex}>
-                                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                        {/** NOTE: you don't have to check for null for the disabled attribute because null is falsey
+                                         *         but I'm just adding it for clarity
+                                         */}
+                                        <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                                            {playerSymbol}
+                                        </button>
                                     </li>
                                 );
                             })}
