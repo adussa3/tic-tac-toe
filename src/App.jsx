a3109f819647558e5dd1e64226ba5fefb9b141ea
adussa3 <adussa3@gmail.com>
2024-02-21 10:50:31 -0800

    Imported all tic-tac-toe winning combinations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import Player from "./components/Player";
+import Log from "./components/Log";
 import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
 import { useState } from "react";
 
-function App() {
-    const [activePlayer, setActivePlayer] = useState("X");
+function deriveActivePlayer(gameTurns) {
+    let activePlayer = "X";
+    if (gameTurns.length > 0 && gameTurns[0].player === "X") activePlayer = "O";
+    return activePlayer;
+}
 
-    const handleSelectSquare = () => {
-        setActivePlayer((prevActivePlayer) => (prevActivePlayer === "X" ? "O" : "X"));
+function App() {
+    const [gameTurns, setGameTurns] = useState([]);
+    const activePlayer = deriveActivePlayer(gameTurns);
+
+    const handleSelectSquare = (rowIndex, colIndex) => {
+        setGameTurns((prevTurns) => {
+            const currentPlayer = deriveActivePlayer(prevTurns);
+            // NOTE: we DON'T want to do "player: activePlayer" because we would be combining two different states
+            // we're not getting the prev value of activePlayer! It's possible that changes made to the state aren't applied
+            // before the re-render
+            return [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
+        });
     };
 
     return (
@@ -16,8 +31,9 @@ function App() {
                     <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
                     <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
                 </ol>
-                <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+                <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
             </div>
+            <Log turns={gameTurns} />
         </main>
     );
 }
