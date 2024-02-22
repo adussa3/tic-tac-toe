2e46e50716da8cb93edc4b07c54fc380e1d8f960
adussa3 <adussa3@gmail.com>
2024-02-22 14:44:45 -0800

    Display Player Name on GameOver screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,20 @@ import Player from "./components/Player";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import { useState } from "react";
 
+const PLAYERS = { X: "Player 1", O: "Player 2" };
+
 function deriveActivePlayer(gameTurns) {
     let activePlayer = "X";
     if (gameTurns.length > 0 && gameTurns[0].player === "X") activePlayer = "O";
     return activePlayer;
 }
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
 
-function App() {
-    const [gameTurns, setGameTurns] = useState([]);
-    const activePlayer = deriveActivePlayer(gameTurns);
-
-    let gameBoard = [...initialGameBoard.map((row) => [...row])];
+function deriveGameBoard(gameTurns) {
+    let gameBoard = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ];
 
     for (const turn of gameTurns) {
         const { square, player } = turn;
@@ -28,6 +26,10 @@ function App() {
         gameBoard[row][col] = player;
     }
 
+    return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
     let winner = null;
 
     for (const combination of WINNING_COMBINATIONS) {
@@ -37,10 +39,20 @@ function App() {
         const thirdSquareSymbol = gameBoard[square3.row][square3.column];
 
         if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
-            winner = firstSquareSymbol;
+            winner = players[firstSquareSymbol];
         }
     }
 
+    return winner;
+}
+
+function App() {
+    const [players, setPlayers] = useState(PLAYERS);
+    const [gameTurns, setGameTurns] = useState([]);
+
+    const activePlayer = deriveActivePlayer(gameTurns);
+    const gameBoard = deriveGameBoard(gameTurns);
+    const winner = deriveWinner(gameBoard, players);
     const hasDraw = gameTurns.length === 9 && !winner;
 
     const handleSelectSquare = (rowIndex, colIndex) => {
@@ -57,12 +69,18 @@ function App() {
         setGameTurns([]);
     };
 
+    const handlePlayerNameChange = (symbol, newName) => {
+        // We can dynamically get the key by using [symbol]
+        // this gets the actual symbol's value!
+        setPlayers((prevPlayers) => ({ ...prevPlayers, [symbol]: newName }));
+    };
+
     return (
         <main>
             <div id="game-container">
                 <ol id="players" className="highlight-player">
-                    <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
-                    <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
+                    <Player initialName={PLAYERS.X} symbol="X" isActive={activePlayer === "X"} onChangeName={handlePlayerNameChange} />
+                    <Player initialName={PLAYERS.Y} symbol="O" isActive={activePlayer === "O"} onChangeName={handlePlayerNameChange} />
                 </ol>
                 {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
                 <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} turns={gameTurns} />
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 
-export default function Player({ initialName = "Player", symbol, isActive }) {
+export default function Player({ initialName = "Player", symbol, isActive, onChangeName }) {
     const [isEditing, setIsEditing] = useState(false);
+
+    // We should NOT lift the playerName state up into App because it's being used by the input
+    // on EVERY keystroke
+    //
+    // If we move into the App component, then the ENTIRE app (the GameBoard and Log) is re-rendered on every keystroke!
+    // This is really redundant and NOT what we want to do!
+    //
+    // Also, it would be tricky, because we call the Player component twice in App and every Player component should
+    // manage it's own playerName
+    //
+    // Instead, we add a new state in the App component, where we store the currently set playerNames
     const [playerName, setPlayerName] = useState(initialName);
 
-    const handleEditClick = () => {
+    const handleEditClick = (symbol) => {
+        // NOTE: when isEditing is true on click, that means we're done editing and we'll set it to false
+        //       then we change the name
+        if (isEditing) onChangeName(symbol, playerName);
         setIsEditing((prevIsEditing) => !prevIsEditing);
     };
 
@@ -24,7 +38,7 @@ export default function Player({ initialName = "Player", symbol, isActive }) {
                 {isEditing && <input type="text" className="player-name" onChange={handleChange} value={playerName} required />}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+            <button onClick={() => handleEditClick(symbol)}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
